842acfdf30457169cc9b70d9b42e837996d5f009
yongsannnn <yong_s@live.com>
2021-02-23 09:46:09 +0000

    added 08-axios-and-life-cycle and did til axios and push element

---
+++ 08-axios-and-life-cycle/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from "./TodoList"
+function App() {
+  return (
+    <div>
+        <TodoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 08-axios-and-life-cycle/src/TodoList.js
@@ -0,0 +1,39 @@
+import React from "react"
+import axios from "axios"
+
+class TodoList extends React.Component {
+    state = {
+        tasks:[]
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <button onClick={this.loadData}>Load Data</button>
+                {this.renderList()}
+            </React.Fragment>
+        )
+    }
+
+    loadData = async () => {
+        // in React using axios.get will start looking for file in the public folder. 
+        let response = await axios.get("items.json");
+        console.log(response.data)
+        this.setState({
+            "tasks": response.data.tasks
+        })
+    }
+
+    renderList = () => {
+        let list = [];
+        for (let task of this.state.tasks) {
+            let todoElement = <div key={task.id}>
+                <h2>{task.title}</h2>
+            </div>
+        list.push(todoElement)
+        }
+        return list
+    }
+}
+
+export default TodoList
