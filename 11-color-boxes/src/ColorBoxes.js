d32e52f1eb32991e183b41f7a81076f4cc6fb116
yongsannnn <yong_s@live.com>
2021-02-24 07:58:30 +0000

    added colorcontrol

--- 11-color-boxes/src/ColorBoxes.js
+++ 11-color-boxes/src/ColorBoxes.js
@@ -1,27 +1,42 @@
 import React from "react"
 import ColorDisplay from "./ColorDisplay"
+// import ColorControl from "./ColorControl"
 
 export default class ColorBoxes extends React.Component {
     state = {
         boxes:[{
             "color": "white"
         },{
-            "color": "white"
+            "color": "red"
         },{
-            "color": "white"
+            "color": "blue"
         },{
-            "color": "white"
+            "color": "green"
         }
-
         ]}
 
     render () {
         return (
             <React.Fragment>
                 <ColorDisplay boxes={this.state.boxes} />
+                {/* <ColorControl boxes={this.state.boxes}/> */}
             </React.Fragment>
 
         )
+    }
+
+    updateBox = (event)=>{
+        let index = event.target.name
+        let clone = this.state.boxes.map(function(eachBox, boxIndex){
+            if (boxIndex !== index){
+                return eachBox
+            } else {
+                return {color:event.target.value}
+            }
+        })
+        this.setState({
+            "boxes" : clone
+        })
     }
 
 }
--- 11-color-boxes/src/ColorDisplay.js
+++ 11-color-boxes/src/ColorDisplay.js
@@ -19,6 +19,4 @@ export default function ColorDisplay(props) {
         </React.Fragment>
 
     )
-
-
 }
