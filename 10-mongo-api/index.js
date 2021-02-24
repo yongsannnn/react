e572adda58cdbf70f123b42eb4465fe5fdbeb6e4
yongsannnn <yong_s@live.com>
2021-02-24 06:34:22 +0000

    added crud in mongo-express

--- 10-mongo-api/index.js
+++ 10-mongo-api/index.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 require("dotenv").config();
 const MongoUtil = require("./MongoUtil");
 const mongoUrl = process.env.MONGO_URL;
+const ObjectId = require("mongodb").ObjectId;
 let app = express ();
 app.use(express.json());
 app.use(cors());
@@ -11,10 +12,68 @@ app.use(cors());
 
 async function main(){
     let db = await MongoUtil.connect(mongoUrl, "tgc-11")
-    console.log("database connected")
+    console.log("Database connected")
 
+    // Get - Fetch information
+    app.get("/task" ,async(req,res)=>{
+        let tasks = await db.collection("tasks").find().toArray();
+        res.status(200)
+        res.send(tasks)
+    })
 
-    
+    // Post - Add new document
+    app.post("/task", async (req,res)=>{
+        let title = req.body.title
+        let done = req.body.done
+
+        try {
+            let results = await db.collection("tasks").insertOne({
+                title : title,
+                done: done
+            })
+            res.status(200)
+            res.send(results)
+        } catch (e) {
+            res.status (500)
+            res.send({
+                "Message" : "Unable to insert"
+            });
+            console.log(e)
+        }
+    })
+
+    // Put 
+    app.put("/task", async (req, res)=>{
+
+        try {
+            await db.collection("tasks").updateOne({
+                "_id": ObjectId(req.body._id)
+            },{
+                "$set":{
+                    "title":req.body.title,
+                    "done":req.body.done
+                }
+            })
+            res.status(200)
+            res.send("Update received")
+        } catch (e){
+            res.status(500)
+            res.send({
+                "Message" : "Unable to update"
+            })
+            console.log(e)
+        }
+    })
+
+    // Delete
+    app.delete("/task/:id", async (req,res)=>{
+        await db.collection("tasks").deleteOne({
+            _id:ObjectId(req.params.id)
+        })
+    })
+    res.status(200);
+    res.send("Deleted the request")
+
 }
 
 main()
