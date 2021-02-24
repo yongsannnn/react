9120050266a5a3b1c6fd563a5d0c0e230b44d1a1
yongsannnn <yong_s@live.com>
2021-02-24 02:33:28 +0000

    Initial Commit + TodoList

---
+++ 09-crud/src/TodoList.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default class TodoList extends React.Component {
+    state = {
+        tasks: [
+            {
+                "id": 1234,
+                "title": "Mop the floor",
+                "done": false
+            }
+        ]
+    }
+    render (){
+        return (
+            
+        )
+    }
+
+}
